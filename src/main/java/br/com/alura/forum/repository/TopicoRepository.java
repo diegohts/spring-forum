5352361a6c7347e52f8c3e06ce5f52e870093b8e
Diego Henrique <diegohrp20@gmail.com>
2022-04-29 09:54:37 -0300

    Important notes from other cases to make the naming of the method that will be more appropriate according to Spring Data.

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -13,5 +13,15 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
                                                     // ?nomeCurso=HMTL 5
                                                     // Com isso traz as informacoes do topico referente a esse Curso
                                                     // filtrado
+                                                    // Agora imagine a situacao Topico tenha um atributo chamado
+                                                    // cursoNome, entao para tirar essa ambiguidade, colocamos o _ para
+                                                    // separar pois queremos o nome da tabela curso nao o atributo
+                                                    // nomeCurso da tabela topico, entao ficaria a tabela_atributo,
+                                                    // desse modo findByCurso_Nome.
 
+    // Esse seria um caso que queira outro nome, o ruim que tenho que montar a query
+    // no annotation, o de cima ja vem como convencao e o Spring Data interpreta e
+    // sendo bem mais facil
+    // @Query("SELECT t FROM Topico t WHERE t.cuso.nome= :nomeCurso")
+    // List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
