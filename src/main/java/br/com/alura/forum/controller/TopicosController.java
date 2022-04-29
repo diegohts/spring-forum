6ae5a4c6edb1307afd4ad3195e79269bc3faab1c
Diego Henrique <diegohrp20@gmail.com>
2022-04-29 11:26:06 -0300

    Structuring the best way to register a topic in the system.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
@@ -37,8 +40,16 @@ public class TopicosController {
     }
 
     @PostMapping
-    public void cadastrar(@RequestBody TopicoForm form) {
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+        // tenho que retornar o codigo 201 o cadastro com sucesso, se fosse void e
+        // processasse com sucesso era 200, nao sendo ideal nesse caso
+
         Topico topico = form.converter(cursoRepository);
         TopicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        // o caminho presente para execucao da criacao de um unico topico e o pega o
+        // topico criado e transforma na uri completinha com endereco do servidor
+        return ResponseEntity.created(uri).body(new TopicoDto(topico)); // o dto como corpo da resposta
     }
 }
