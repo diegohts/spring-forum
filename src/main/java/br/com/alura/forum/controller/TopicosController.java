f39c82846e14be7ca5f00e78af3ea6adbc0d51bd
Diego Henrique <diegohrp20@gmail.com>
2022-04-29 16:54:31 -0300

    Adding the bean validation, to validate the information passed by the user through a form, which registers a topic in the system.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +21,11 @@ import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
-@RequestMapping(value = "/topicos")
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
-    private TopicoRepository TopicoRepository;
+    private TopicoRepository topicoRepository;
 
     @Autowired
     private CursoRepository cursoRepository;
@@ -31,21 +33,32 @@ public class TopicosController {
     @GetMapping
     public List<TopicoDto> lista(String nomeCurso) {
         if (nomeCurso == null) {
-            List<Topico> topicos = TopicoRepository.findAll();
+            List<Topico> topicos = topicoRepository.findAll();
             return TopicoDto.converter(topicos);
         } else {
-            List<Topico> topicos = TopicoRepository.findByCursoNome(nomeCurso);
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
             return TopicoDto.converter(topicos);
         }
     }
 
     @PostMapping
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
+        // O valid quer dizer a Spring que aundo for injetar essa TopicoForm puxando os
+        // dados que estão vindo na requisição, chame tambem @Valid as suas validacoes
 
+        // Aqui poderia fazer as validacoes cheio de if e else retornando o codigo
+        // especifico para cada situacao, porem no spring form isso nao eh uma boa
+        // solucao, pois poluiria o codigo fonte. Entao usa-se o BeanValidation para
+        // reliazar as validacoes de forma mais limpa.
+
+        // E as validacoes acontecem quando o usuario vai cadastrar as informacoes
+        // atraves do formulario passado, no TopicoForm. Entao passaremos as anotacoes
+        // para fazer essas validacoes
         Topico topico = form.converter(cursoRepository);
-        TopicoRepository.save(topico);
+        topicoRepository.save(topico);
 
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new TopicoDto(topico));
     }
+
 }
--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,41 +1,52 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+    @NotNull
+    @NotEmpty
+    @Length(min = 5)
     private String titulo;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 10)
     private String mensagem;
+
+    @NotNull
+    @NotEmpty
     private String nomeCurso;
 
-    /**
-     * @return String return the titulo
-     */
     public String getTitulo() {
         return titulo;
     }
 
-    /**
-     * @param titulo the titulo to set
-     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
-    /**
-     * @return String return the mensagem
-     */
     public String getMensagem() {
         return mensagem;
     }
 
-    /**
-     * @param mensagem the mensagem to set
-     */
     public void setMensagem(String mensagem) {
         this.mensagem = mensagem;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
     }
 
     public Topico converter(CursoRepository cursoRepository) {
