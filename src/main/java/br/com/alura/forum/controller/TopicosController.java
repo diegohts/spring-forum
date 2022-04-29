5ca996e0ff43a8f05162fc192375e2faa0d276af
Diego Henrique <diegohrp20@gmail.com>
2022-04-29 09:05:02 -0300

    Perform search filtering as an example, filter through the titulo attribute of the Topico table.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -17,10 +17,26 @@ public class TopicosController {
     private TopicoRepository TopicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista() {
-        List<Topico> topicos = TopicoRepository.findAll();
-
-        return TopicoDto.converter(topicos);
+    public List<TopicoDto> lista(String titulo) { // No modo anterior trazia todos os registros. Às vezes queremos
+                                                  // filtrar. Filtrando nesse caso pelo Titulo um atributo do
+                                                  // TopicoDto
+        if (titulo == null) { // se nao esta vindo o parâmetro titulo vou chamar o findAll
+            List<Topico> topicos = TopicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = TopicoRepository.findByTitulo(titulo); // Se você seguir esse padrão, ele
+                                                                          // consegue gerar a Query para você
+                                                                          // automaticamente.
+                                                                          // O padrão de nomenclatura seria findBy()
+                                                                          // e o nome do atributo na entidade (do
+                                                                          // atributo que você quer filtrar). Por
+                                                                          // exemplo, imagine que quero filtrar pelo
+                                                                          // Titulo, o nome do método seria
+                                                                          // findByTitulo(titulo) e eu passaria
+                                                                          // o parâmetro que representa esse
+                                                                          // atributo.
+            return TopicoDto.converter(topicos);
+        }
 
     }
 }
