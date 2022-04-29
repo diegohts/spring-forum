66743210724783cbf19189cc67d9a6d321be6780
Diego Henrique <diegohrp20@gmail.com>
2022-04-29 00:25:24 -0300

    Deleting comments.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -14,28 +14,13 @@ import br.com.alura.forum.repository.TopicoRepository;
 public class TopicosController {
 
     @Autowired
-    private TopicoRepository TopicoRepository; // preciso injetar o JpaRepository que acabei de criar. Para fazer a
-                                               // injeção, uso o mesmo esquema do Spring @Autowired. Você declara um
-                                               // atributo, que no nosso caso vai ser do tipo topicoRepository, que é
-                                               // nossa interface, e aí vou chamar de topicoRepository o atributo. Vou
-                                               // só importar o @Autowired e o TopicoRepository. Está pronta a injeção
-                                               // de dependências como em qualquer outra classe.
+    private TopicoRepository TopicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista() { // preciso que retorne a lista de topicos com os atributos mencionados no
-                                     // TopicoDto
-        List<Topico> topicos = TopicoRepository.findAll(); // Quando injeto o topicoRepository e chamo o método
-                                                           // findAll(), que nem fomos nós que escrevemos - se olharmos
-                                                           // lá no nosso TopicoRepository.java, está vazio, porque ele
-                                                           // já veio herdado do JpaRepository. Essa é uma comodidade
-                                                           // para quem trabalha com JPA, não precisar criar mais as
-                                                           // classes seguindo o padrão DAO. Usa o Repository e deixa o
-                                                           // seu código muito menor, muito mais simples e fácil de
-                                                           // fazer manutenção.
+    public List<TopicoDto> lista() {
+        List<Topico> topicos = TopicoRepository.findAll();
 
-        return TopicoDto.converter(topicos); // Na hora de chamar o TopicoDto.converter(), preciso passar uma lista
-                                             // chamada topicos, que busca todos os topicos no banco mostrando atraves
-                                             // do TopicoDto os atributos que quero mostrar
+        return TopicoDto.converter(topicos);
 
     }
 }
