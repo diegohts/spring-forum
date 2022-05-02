f2a267a242691ebc93f5dbca2e1c0937d1e13690
Diego Henrique <diegohrp20@gmail.com>
2022-05-02 10:57:50 -0300

    Simplifying the pagination performed with Spring Boot.

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,11 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport // habilita o suporte para o String pegar da requisição dos parametros da url os
+							// campos de paginação e ordenação e passar isso para o Spring Data
 public class ForumApplication {
 
 	public static void main(String[] args) {
