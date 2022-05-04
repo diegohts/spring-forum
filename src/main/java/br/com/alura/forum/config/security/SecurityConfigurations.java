6b6767a5e4b08c896d4bb93653d6ffc686eb39b6
Diego Henrique <diegohrp20@gmail.com>
2022-05-03 23:45:13 -0300

    Monitoring with Spring Boot Actuator. An API that has endpoints that return a JSON with information about the API. It is a facility to be able to monitor and follow the progress of our API.

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -47,6 +47,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll() // liberar o actuator para ver info do projeto
+                // actuator tem informações sensiveis, entao so para pessoas permitidas
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
